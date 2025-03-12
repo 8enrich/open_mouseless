74f54c9cc2081f89f91a3ec598369d6a05d62df4
8enrich <henriqueftcunha@gmail.com>
2025-03-11 22:01:49 -0300

    Melhorias de performance

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import sys
 from filelock import FileLock
+from PyQt5.QtGui import QCursor
+from PyQt5.QtCore import QPoint
 from pyautogui import click, doubleClick, dragTo
-from PyQt5.QtWidgets import QApplication, QDesktopWidget, QWidget, QLabel
-from PyQt5.QtCore import QPoint, QRectF, Qt, QTimer
-from PyQt5.QtGui import QPainter, QColor, QBrush, QFont, QCursor, QPixmap
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtCore import QRectF, Qt, QTimer
+from PyQt5.QtGui import QPainter, QColor, QFont, QPixmap, QTransform
 from os import path
 from json import load
 
@@ -13,49 +15,49 @@ class OpenMouseless(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Mouseless")
-        self.alphabet = {key: value for value, key in enumerate("ABCDEFGHIJKLMNOPQRSTUVWXYZ")}
+        self.alphabet = list(map(chr, range(65, 65 + 26)))
         self.numbers = "01234"
-        self.functions = {
-            "0": (click, {}), 
-            "1": (click, {"button":'right'}),
-            "2": (doubleClick, {}),
-            "3": (click, {"clicks": 3})
-        }
-        self.actions_text = {
-            "0": "Click",
-            "1": "Right click",
-            "2": "Double click",
-            "3": "Triple click",
-            "4": "Hold"
-        }
-
-        with open(SETTINGS_FILE_PATH, 'r') as file:
-            settings = load(file)
-        language = settings.get("language", "EN")    
 
         self.keyboard_layout = [
             ['q', 'w', 'e', 'r', 'u', 'i', 'o', 'p'],
-            ['a', 's', 'd', 'f', 'j', 'k', 'l', ('ç' if "BR" in language else ';')],
-            ['z', 'x', 'c', 'v', 'm', ',', '.', (';' if "BR" in language else '?')]
+            ['a', 's', 'd', 'f', 'j', 'k', 'l', ';'],
+            ['z', 'x', 'c', 'v', 'm', ',', '.', '?']
         ]
         self.inner_cols = len(self.keyboard_layout[0])
         self.inner_rows = len(self.keyboard_layout)
-        self.keyboard_positions = self._create_keyboard_map()
         
         self.buffer_pixmap = None
         self.flash_timer = None
         self.cell_width = 0
         self.cell_height = 0
-        
         self._setup_window()
-        self._init_flash_label()
+        self.flash_label = None
         self.reset_selection()
+        self.create_static_texts()
+        QTimer.singleShot(0, self.post_init_optimizations)
+        QTimer.singleShot(0, self.load_settings_async)
+
+    def load_settings_async(self):
+        try:
+            with open(SETTINGS_FILE_PATH, 'r') as file:
+                settings = load(file)
+            language = settings.get("language", "EN")
+            
+            self.keyboard_layout[1][-1] = 'ç' if "BR" in language else ';'
+            self.keyboard_layout[2][-1] = ';' if "BR" in language else '?'
+            self.keyboard_positions = self._create_keyboard_map()
+        except Exception as e:
+            print("Erro ao carregar configurações:", e)
+
+    def post_init_optimizations(self):
+        self._update_buffer()
+        self.update()
 
     def _setup_window(self):
         self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint | Qt.Tool)
         self.setAttribute(Qt.WA_TranslucentBackground)
         self.setAttribute(Qt.WA_TransparentForMouseEvents)
-        screen = QDesktopWidget().screenGeometry()
+        screen = QApplication.primaryScreen().geometry()
         self.setGeometry(0, 0, screen.width(), screen.height())
 
     def _create_keyboard_map(self):
@@ -80,36 +82,53 @@ class OpenMouseless(QWidget):
         super().resizeEvent(a0)
         self.cell_width = self.width() / 26
         self.cell_height = self.height() / 26
-        self._update_buffer()
+        self.update_grid_lines()
+        self._update_buffer()  
+
+    def create_static_texts(self):
+        from PyQt5.QtGui import QStaticText
+        self.static_texts = [QStaticText(letter) for letter in self.alphabet]
+        for static_text in self.static_texts:
+            static_text.prepare(QTransform(), QFont('Arial', 14, QFont.Bold))
+
+    def update_grid_lines(self):
+        self.vertical_lines = [(int(i * self.cell_width), 0, int(i * self.cell_width), self.height()) for i in range(27)]
+        self.horizontal_lines = [(0, int(i * self.cell_height), self.width(), int(i * self.cell_height)) for i in range(27)]
 
     def _update_buffer(self):
-        """Atualiza o buffer apenas com elementos estruturais"""
+        """Atualiza o buffer usando linhas para melhor performance"""
         self.buffer_pixmap = QPixmap(self.size())
         self.buffer_pixmap.fill(Qt.transparent)
         
         painter = QPainter(self.buffer_pixmap)
         painter.setRenderHint(QPainter.Antialiasing)
         
-        square_color = QColor(0, 0, 0, 25)
-        border_color = QColor(255, 255, 255, 25)
+        line_color = QColor(255, 255, 255, 50)
+        painter.setPen(line_color)
+        for line in self.vertical_lines:
+            painter.drawLine(*line)
+        for line in self.horizontal_lines:
+            painter.drawLine(*line)
 
-        inner_square_width = self.cell_width / self.inner_cols
-        inner_square_height = self.cell_height / self.inner_rows
+        
+        sub_line_color = QColor(0, 0, 0, 25)
+        painter.setPen(sub_line_color)
+
+        cell_width_over_inner_cols = self.cell_width / self.inner_cols
+        cell_height_over_inner_rows = self.cell_height / self.inner_rows
+        
+        
+        for col in range(26):
+            for i in range(1, self.inner_cols):
+                x = col * self.cell_width + i * cell_width_over_inner_cols
+                painter.drawLine(int(x), 0, int(x), self.height())
+        
         
         for row in range(26):
-            for col in range(26):
-                x = col * self.cell_width
-                y = row * self.cell_height
-                
-                painter.setPen(border_color)
-                cell_rect = QRectF(x, y, self.cell_width, self.cell_height)
-                painter.drawRect(cell_rect)
-                
-                for i in range(self.inner_cols):
-                    for j in range(self.inner_rows):
-                        painter.setBrush(QBrush(square_color))
-                        inner_rect = QRectF(x + i * inner_square_width, y + j * inner_square_height, inner_square_width, inner_square_height)
-                        painter.drawRect(inner_rect)
+            for j in range(1, self.inner_rows):
+                y = row * self.cell_height + j * cell_height_over_inner_rows
+                painter.drawLine(0, int(y), self.width(), int(y))
+
         painter.end()
 
     def paintEvent(self, a0):
@@ -124,28 +143,27 @@ class OpenMouseless(QWidget):
         
         painter.end()
 
-    def _draw_main_letters(self, painter):
-        painter.setFont(QFont('Arial', 14, QFont.Bold))
-        painter.setPen(QColor(255, 255, 255, 150))
-        alphabet = list(self.alphabet.keys())
-
-        cell_height_over_5 = self.cell_height/5
-        half_of_cell_width = self.cell_width/2
-        half_of_cell_height = self.cell_height/2
-        
+    def precompute_letter_positions(self):
+        self.letter_positions = []
+        cell_text_offset = self.cell_height / 5  
         for row in range(26):
             for col in range(26):
                 x = col * self.cell_width
-                y = row * self.cell_height
-                
-                painter.drawText(
-                    QRectF(x, y + cell_height_over_5, half_of_cell_width, half_of_cell_height),
-                    Qt.AlignCenter, alphabet[row]
-                )
-                painter.drawText(
-                    QRectF(x + half_of_cell_width, y + cell_height_over_5, half_of_cell_width, half_of_cell_height),
-                    Qt.AlignCenter, alphabet[col]
-                )
+                y = row * self.cell_height + cell_text_offset
+                self.letter_positions.append((x, y))
+
+    def _draw_main_letters(self, painter):
+        painter.setFont(QFont('Arial', 14, QFont.Bold))
+        painter.setPen(QColor(255, 255, 255, 150))
+        
+        if not hasattr(self, 'letter_positions'):
+            self.precompute_letter_positions()
+        
+        for index, (x, y) in enumerate(self.letter_positions):
+            letter1 = self.alphabet[index // 26]
+            letter2 = self.alphabet[index % 26]
+            painter.drawText(QRectF(x, y, self.cell_width/2, self.cell_height/2), Qt.AlignCenter, letter1)
+            painter.drawText(QRectF(x + self.cell_width/2, y, self.cell_width/2, self.cell_height/2), Qt.AlignCenter, letter2)
 
     def _draw_subgrid(self, painter):
         cell_x = self.selected_col * self.cell_width
@@ -168,7 +186,11 @@ class OpenMouseless(QWidget):
                 except IndexError:
                     pass
 
-    def show_flash_message(self, message):
+    def show_flash_message(self):
+        if self.flash_label is None:
+            self._init_flash_label()
+        actions_text = ["Click", "Right click", "Double click", "Triple click", "Hold"]
+        message = actions_text[self.action]
         self.flash_label.setText(message)
         self.flash_label.adjustSize()
         self.flash_label.move(
@@ -211,16 +233,22 @@ class OpenMouseless(QWidget):
         self.letter3 = ""
         self.selected_row = -1
         self.selected_col = -1
-        self.action = "0"
+        self.action = 0
         self.hold = False
+    
+    def get_letter_code(self, letter):
+        return ord(letter.upper()) - 65
 
     def handle_first_two_letters(self, event):
         key = event.text().upper()
         if key in self.numbers and not self.hold:
-            self.action = key
-            self.show_flash_message(self.actions_text[self.action])
+            try:
+                self.action = int(key)
+            except ValueError:
+                return
+            self.show_flash_message()
             return
-        if key not in self.alphabet.keys():
+        if key not in self.alphabet:
             return
 
         if not self.letter1:
@@ -228,8 +256,8 @@ class OpenMouseless(QWidget):
             return
         if not self.letter2:
             self.letter2 = key
-            self.selected_row = self.alphabet[self.letter1]
-            self.selected_col = self.alphabet[self.letter2]
+            self.selected_row = self.get_letter_code(self.letter1)
+            self.selected_col = self.get_letter_code(self.letter2)
 
     def get_third_letter(self, event):
         if event.key() == Qt.Key_Space:
@@ -262,16 +290,24 @@ class OpenMouseless(QWidget):
             dragTo(global_pos.x(), global_pos.y())
         else:
             QCursor.setPos(global_pos)
-            if self.action == "4":
+            if self.action == 4:
                 self.reset_selection()
                 self.hold = True
                 return
             self.hide()
-            function, args = self.functions[self.action]
+            functions = [
+                (click, {}), 
+                (click, {"button":'right'}),
+                (doubleClick, {}),
+                (click, {"clicks": 3})
+            ]
+            function, args = functions[self.action]
             function(**args)
         QApplication.quit()
 
 if __name__ == "__main__":
+    QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
+    QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps, True)
     lock_file = ".open_mouseless.lock"
 
     lock = FileLock(lock_file)
